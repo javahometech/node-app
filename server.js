45991b0aaa084d12baf7e0da4390d79cf8e15a37
kammana <kammana@javahomecloud.local>
2018-03-20 10:31:18 +0530

    node app for docker

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world from Docker</h1> \n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
