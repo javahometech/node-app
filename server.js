1f33c9fb5e36cdccf5cd221bdc4fb3f328db78e3
javahometech <kammana.hari@gmail.com>
2018-03-23 09:29:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world from Docker second deployment</h1> \n');
+  res.send('<h1>Doker Swarm node-app version 3</h1> \n');
 });
 
 app.listen(PORT, HOST);
