915c6f72e357c11cd7c578fde3a17fd68b8595f6
javahometech <kammana.hari@gmail.com>
2019-07-18 19:31:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">Java Home - Version 5 - Running on Docker!!!!!!!!</h1> \n');
+  res.send('<h1 style="color:red;">Java Home - Version 6 - Running on Docker!!!!!!!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
