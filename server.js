8ede2a424f5d6162f2c31ea9c1b4a5a663e203be
javahometech <kammana.hari@gmail.com>
2019-08-01 10:48:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">Java Home App - CI/CD Jenkins!!</h1> \n');
+  res.send('<h1 style="color:red;">Java Home App - CI/CD Jenkins123!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
