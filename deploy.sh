0b7c4e6fd452e0c587ce7a3592ab6e7ce05d5680
javahometech <kammana.hari@gmail.com>
2019-08-02 10:17:15 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+node_app=`docker ps -a | grep nodeapp | awk '{print $NF}'`
+if [ $node_app=='nodeapp' ]; then
+    echo "nodeapp is running, lets delete"
+	docker rm -f nodeapp
+fi
+
+docker run -d -p 8080:8080 --name $1
