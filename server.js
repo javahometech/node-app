008cc2e96a3cb2680a53d9a146969153943bc906
javahometech <kammana.hari@gmail.com>
2019-10-05 12:13:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:black;">Java Home App - Kubernetes - version-2!!</h1> \n');
+  res.send('<h1 style="color:red;">Java Home App - version-3!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
