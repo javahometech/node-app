eacec1bdee734de6c79b7913c487f9cefa3c8f36
javahometech <kammana.hari@gmail.com>
2019-10-12 11:22:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Java Home App - version-4!!</h1> \n');
+  res.send('<h1 style="color:red;">Java Home App - version-5!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
