caf33cee20e4a4314d852a51859f90ed5dc8a5a1
DibyoGit <64616330+DibyoGit@users.noreply.github.com>
2020-07-23 09:55:45 +0530

    Update server.js
    response show host and port

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Java Home App - version-10!!</h1> \n');
-});
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+app.get('/', (req, res) => {
+  res.send('<h1 style="color:red;">Java Home App - version-10!! Running on http://${HOST}:${PORT}` </h1> \n');
+});
