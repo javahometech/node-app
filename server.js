806ab7cac32bf9dedc6ef6cd1634ec2d621b55e2
prakas876 <prakas876@gmail.com>
2022-02-24 05:03:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Java Home App - version-10!!</h1> \n');
+  res.send('<h1 style="color:red;">Java Home App - version-10!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
